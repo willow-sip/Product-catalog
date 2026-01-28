588a3167dcbe0ceaef304f3b77243aa7071dbb0b
Diana Loyuk <wyrrrth.sip@gmail.com>
2026-01-28 20:36:13 +0300

    feat: added API usage

---
+++ src/ProductApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { Product } from './types/product';
+
+const api = axios.create({
+  baseURL: 'https://fakestoreapi.com',
+  timeout: 10000,
+});
+
+export const productService = {
+  async getAllProducts(): Promise<Product[]> {
+    const response = await api.get<Product[]>('/products');
+    return response.data;
+  },
+
+  async getProductById(id: number): Promise<Product> {
+    const response = await api.get<Product>(`/products/${id}`);
+    return response.data;
+  }
+};
